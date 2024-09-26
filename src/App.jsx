fdaa62cf5681654e9f6f9931afa58a6c93848092
MoranSin <nurixhbh@gmail.com>
2024-09-26 18:06:51 +0300

    feat: a litlle styling and fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,46 @@ import React, { useState, useEffect } from 'react';
 import { useAuth, useLoginWithRedirect, ContextHolder, AdminPortal, useAuthActions } from "@frontegg/react";
 
 function App() {
-  const [tenants, setTenants] = useState([]);
-  const { switchTenant } = useAuthActions();
   const { user, isAuthenticated } = useAuth();
   const loginWithRedirect = useLoginWithRedirect();
-
-  useEffect(() => {
-    fetchTenants();
-  }, []);
 
   const logout = () => {
     const baseUrl = ContextHolder.getContext().baseUrl;
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   };
 
-  const handleSwitchTenant = () => { 
-    switchTenant({ tenantId: user.tenantId });
-};
-
   const openAdminPortal = () => {
     AdminPortal.show();
   };
 
-  const fetchTenants = () => {
-
-  }
-
   return (
-    <div className="App">
+    <div>
       { isAuthenticated ? (
-        <div>
+        <div className='container'>
           <div>
             <img src={user?.profilePictureUrl} alt={user?.name}/>
           </div>
           <div>
-            <span>Hello {user?.name}!</span>
+            <span className='loginHeader'>Hello {user?.name}!</span>
           </div>
+          <div className='buttonsContainer'>
           <div>
             <button onClick={() => logout()}>Logout</button>
           </div>
           <div>
-            <button onClick={() => openAdminPortal()}>Settings</button>
+            <button onClick={() => openAdminPortal()}>
+              <img className='settings' src='../public/settings.png' alt='settings'/>
+            </button>
           </div>
-          <div>
-      <label htmlFor="tenantDropdown">Switch Tenant:</label>
-      <select
-        id="tenantDropdown"
-        value={activeTenant || ''}
-        onChange={(e) => handleTenantChange(e.target.value)}
-      >
-        <option value="" disabled>Select a tenant</option>
-        {tenants.map((tenant) => (
-          <option key={tenant.id} value={tenant.id}>
-            {tenant.name}
-          </option>
-        ))}
-      </select>
-    </div>
-          {/* <div>
-            <button onClick={() => handleSwitchTenant()}>Tenant List</button>
-          </div> */}
+          </div>
         </div>
       ) : (
         <div>
-          <button onClick={() => loginWithRedirect()}>Login</button>
+          <img className='fronteggLogo' src='../public/fronteggLogo.png' alt='Frontegg Logo'/>
+          <div className='container'>
+            <span className='loginHeader'>Hello Guest!</span>
+            <button onClick={() => loginWithRedirect()}>Login</button>
+          </div>
         </div>
       )}
     </div>
